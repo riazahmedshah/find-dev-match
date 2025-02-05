4f4a2d98ba2c41139341be7d0a144b525f36a765
riazahmedshah <riyazsh360@gmail.com>
2025-02-05 19:44:11 +0530

    db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
 import express from "express"
-
+import {connectDB} from "./config/db"
 const app = express();
+
 const PORT = 1100;
-app.listen(PORT,() => {
-  console.log(`Serever is running on http://localhost:${PORT}`);
+
+connectDB().then(() => {
+  console.log("DataBase connected successfully!")
+  app.listen(PORT,() => {
+    console.log(`Serever is running on http://localhost:${PORT}`);
+  })
+}).catch((err) => {
+  console.error("DataBase Connection Failed!");
 })
+
