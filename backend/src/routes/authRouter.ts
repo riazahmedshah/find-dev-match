2de1a138ee1f1d8c7222515c0fba1238222af850
riazahmedshah <riyazsh360@gmail.com>
2025-02-08 01:28:46 +0530

    logout api

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -94,3 +94,9 @@ authRouter.post("/signin", async(req, res) => {
     }
   }
 });
+
+authRouter.post("/logout" ,async(req,res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now())
+  }).send("successfully logout")
+})
