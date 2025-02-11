ca77c9dc70dbc120184b14f1f5b55dbddad553bc
riazahmedshah <riyazsh360@gmail.com>
2025-02-11 22:53:35 +0530

    nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { Appbar } from './components/daisiui/Appbar'
 import './index.css'
 
 function App() {
 
   return (
     <div>
+      <Appbar/>
       <h1 className="btn">
         Hello world!
     </h1>
