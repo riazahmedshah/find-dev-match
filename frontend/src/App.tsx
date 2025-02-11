005648c1be5e8dafe06e87b72352b16298bc78b1
riazahmedshah <riyazsh360@gmail.com>
2025-02-11 22:11:36 +0530

    tailwind init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
-      <h1>Hello Dev-Tinder</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     </>
   )
 }
