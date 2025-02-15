5138da4faa976325905afce3f07777fa63cba38b
riazahmedshah <riyazsh360@gmail.com>
2025-02-15 18:40:54 +0530

    cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,16 @@ import express from "express"
 import { mainRouter } from "./routes/index";
 import {connectDB} from "./config/db"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 const app = express();
 
 const PORT = 1100;
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}));
 
 app.use("/", mainRouter);
 
