4b3c3b68bdc7106c994ce935b716f41dfe1180f1
riazahmedshah <riyazsh360@gmail.com>
2025-07-17 21:43:50 +0530

    completed responseHandler

---
+++ backend/src/utils/ResponseHandler.ts
@@ -0,0 +1,35 @@
+import { Response } from "express";
+import { ZodIssue } from "zod";
+
+export class ResponseHandler{
+  static async json(res:Response, data:Record<string, any>, status=200){
+    if(data){
+      res.status(status).json(data)
+    } else{
+      res.status(status).send()
+    }
+  }
+
+  static async created(res:Response, data:Record<string, unknown>){
+    ResponseHandler.json(res, data,201)
+  }
+
+  static unauthorized(res: Response) {
+    res.status(401).send('Unauthorized')
+  }
+
+  static notFound(res: Response, message?: string) {
+    res.status(404).send(message || 'Not Found')
+  }
+
+  static zodError(res:Response, issues:ZodIssue[]){
+    const error = issues.reduce((acc: Record<string,string>, issue) => {
+      const key = issue.path.join(".")
+      acc[key] = issue.message
+
+      return acc;
+    },{});
+
+    res.status(400).json({ error })
+  }
+}
